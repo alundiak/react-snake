f04f6621cd2ccb38ced47678b49e79a6344d7dfa
Andrii Lundiak <landike@gmail.com>
2023-03-25 20:20:29 +0100

    Fixed test

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="">
-      </header>
+      <header className="">Snake</header>
       <main>
         <SnakeGame />
       </main>
